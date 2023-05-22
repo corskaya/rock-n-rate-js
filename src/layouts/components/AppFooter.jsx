f6f7835514796ecd26e9f48d534d6f5a855416c7
Cagri <cagriorskaya@hotmail.com>
2023-05-22 22:45:42 +0300

    Fix header to the top and footer to the bottom

--- src/layouts/components/AppFooter.jsx
+++ src/layouts/components/AppFooter.jsx
@@ -3,12 +3,14 @@ import styles from "./AppFooter.module.css";
 
 function AppFooter() {
   return (
-    <div className={styles.footerContainer}>
-      <div>Music Ratings © - 2023</div>
-      <div className={styles.separator}>-</div>
-      <Link className={styles.footerLink} to="/contact">
-        Contact
-      </Link>
+    <div className={styles.footerFix}>
+      <div className={styles.footerContainer}>
+        <div>Music Ratings © - 2023</div>
+        <div className={styles.separator}>-</div>
+        <Link className={styles.footerLink} to="/contact">
+          Contact
+        </Link>
+      </div>
     </div>
   );
 }
--- src/layouts/components/AppHeader.jsx
+++ src/layouts/components/AppHeader.jsx
@@ -37,58 +37,60 @@ function AppHeader() {
   const location = useLocation();
 
   return (
-    <div className={styles.headerContainer}>
-      <div className={styles.leftPart}>
-        <Link className={styles.logoContainer} to="/">
-          <img className={styles.logo} src={logo} alt="Logo" />
-        </Link>
-        <h4 className={styles.description}>
-          Rate your artist, album and song.
-        </h4>
-      </div>
-      <div className={styles.rightPart}>
-        <div className={styles.navInputContainer}>
-          <AiOutlineSearch className={styles.navInputIcon} />
-          <input className={styles.navInput} placeholder="Quick search" />
+    <div className={styles.headerFix}>
+      <div className={styles.headerContainer}>
+        <div className={styles.leftPart}>
+          <Link className={styles.logoContainer} to="/">
+            <img className={styles.logo} src={logo} alt="Logo" />
+          </Link>
+          <h4 className={styles.description}>
+            Rate your artist, album and song.
+          </h4>
         </div>
-        <nav className={styles.navContainer}>
-          {secondaryNavLinks.map((navLink) => (
-            <Link
-              key={navLink.path}
-              className={`${styles.navLink} ${
-                location.pathname === navLink.path
-                  ? styles.navLinkSelected
-                  : styles.navLinkSecondary
-              }`}
-              to={navLink.path}
-            >
-              {navLink.label}
-            </Link>
-          ))}
-          <div>
-            {primaryNavLinks.map((navLink, index) => (
-              <>
-                <Link
-                  className={`${styles.navLink} ${
-                    location.pathname === navLink.path
-                      ? styles.navLinkSelected
-                      : styles.navLinkPrimary
-                  }`}
-                  to={navLink.path}
-                >
-                  {navLink.label}
-                </Link>
-                {index !== primaryNavLinks.length - 1 && (
-                  <span
-                    className={`${styles.navLink} ${styles.navLinkSecondary}`}
-                  >
-                    |
-                  </span>
-                )}
-              </>
-            ))}
+        <div className={styles.rightPart}>
+          <div className={styles.navInputContainer}>
+            <AiOutlineSearch className={styles.navInputIcon} />
+            <input className={styles.navInput} placeholder="Quick search" />
           </div>
-        </nav>
+          <nav className={styles.navContainer}>
+            {secondaryNavLinks.map((navLink) => (
+              <Link
+                key={navLink.path}
+                className={`${styles.navLink} ${
+                  location.pathname === navLink.path
+                    ? styles.navLinkSelected
+                    : styles.navLinkSecondary
+                }`}
+                to={navLink.path}
+              >
+                {navLink.label}
+              </Link>
+            ))}
+            <div>
+              {primaryNavLinks.map((navLink, index) => (
+                <>
+                  <Link
+                    className={`${styles.navLink} ${
+                      location.pathname === navLink.path
+                        ? styles.navLinkSelected
+                        : styles.navLinkPrimary
+                    }`}
+                    to={navLink.path}
+                  >
+                    {navLink.label}
+                  </Link>
+                  {index !== primaryNavLinks.length - 1 && (
+                    <span
+                      className={`${styles.navLink} ${styles.navLinkSecondary}`}
+                    >
+                      |
+                    </span>
+                  )}
+                </>
+              ))}
+            </div>
+          </nav>
+        </div>
       </div>
     </div>
   );
