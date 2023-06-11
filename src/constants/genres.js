e503022d944a29ba49e6e036dd4fdccccc8dbae9
Cagri <cagriorskaya@hotmail.com>
2023-06-11 23:55:48 +0300

    Add music genres as constants

---
+++ src/constants/genres.js
@@ -0,0 +1,20 @@
+const genres = Object.freeze({
+  african: "African",
+  asian: "Asian",
+  southeastAsian: "Southeast Asian",
+  avantGarde: "Avant-garde",
+  blues: "Blues",
+  caribbean: "Caribbean",
+  country: "Country",
+  easyListening: "Easy listening",
+  electronic: "Electronic",
+  folk: "Folk",
+  hiphop: "Hip hop",
+  jazz: "Jazz",
+  latin: "Latin",
+  pop: "Pop",
+  rnb: "R&B and soul",
+  rock: "Rock",
+});
+
+export default genres;
