b199b2904773bbd71807f676532b2c9742a55b0a
Cagri <cagriorskaya@hotmail.com>
2023-07-10 23:36:58 +0300

    Imrove styles of artist page

--- src/pages/artist/Index.jsx
+++ src/pages/artist/Index.jsx
@@ -4,7 +4,7 @@ import styles from "./styles.module.css";
 import { useEffect } from "react";
 import { getArtist, getSimilarArtists } from "./slice";
 import { StarFilled } from "@ant-design/icons";
-import { Tooltip, Button } from "../../components";
+import { Tooltip, Button, Label } from "../../components";
 
 function Artist() {
   const { id } = useParams();
@@ -51,11 +51,33 @@ function Artist() {
             <h3 className={`${styles.genres} ${styles.textShadow}`}>
               {artist.genres.join(" / ")}
             </h3>
-            <div className={styles.ratingContainer}>
-              <StarFilled className={styles.ratingIcon} />
-              <div className={styles.ratingText}>{`${
-                artist.rating !== 0 ? artist.rating : "?"
-              } / 10`}</div>
+            <div className={styles.ratingsContainer}>
+              <div className={styles.ratingContainer}>
+                <Label className={`${styles.ratingText} ${styles.textShadow}`}>
+                  R'NR RATING
+                </Label>
+                <div>
+                  <div className={styles.ratingPointContainer}>
+                    <StarFilled className={styles.ratingIcon} />
+                    <div className={styles.ratingPoint}>
+                      {artist.rating !== 0 ? artist.rating : "?"}
+                    </div>
+                    <div className={styles.ratingMax}>/ 10</div>
+                  </div>
+                </div>
+              </div>
+              <div className={styles.ratingContainer}>
+                <Label className={`${styles.ratingText} ${styles.textShadow}`}>
+                  YOUR RATING
+                </Label>
+                <div>
+                  <div className={styles.ratingPointContainer}>
+                    <StarFilled className={styles.userRatingIcon} />
+                    <div className={styles.ratingPoint}>8</div>
+                    <div className={styles.ratingMax}>/ 10</div>
+                  </div>
+                </div>
+              </div>
             </div>
             <p className={styles.aboutText}>{artist.about}</p>
           </div>
