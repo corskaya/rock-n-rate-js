6e6af16bcc5fc846d26857e5e8f2039e2712098b
Cagri <cagriorskaya@hotmail.com>
2023-08-10 23:51:08 +0300

    Redirect to album list of related artist

--- src/pages/artist/components/Visuals.jsx
+++ src/pages/artist/components/Visuals.jsx
@@ -1,24 +1,27 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Button } from "../../../components";
 import styles from "../styles.module.css";
+import { setFilters } from "../../albums/slice";
+import { useNavigate } from "react-router-dom";
 
 function Visuals() {
-  const {
-    // artistPending,
-    // artistRejected,
-    // artistFulfilled,
-    artist,
-    // errorMessage,
-    // similarArtistsFulfilled,
-    // similarArtists,
-  } = useSelector((state) => state.artist);
+  const { artist } = useSelector((state) => state.artist);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleViewAlbums = () => {
+    dispatch(setFilters({ searchTerm: artist.name }));
+    navigate("/albums");
+  };
 
   return (
     <div className={styles.visualsContainer}>
       <div className={styles.imageContainer}>
         <img className={styles.image} src={artist.image} alt={artist._id} />
       </div>
-      <Button className={styles.visualButton}>View Albums</Button>
+      <Button className={styles.visualButton} onClick={handleViewAlbums}>
+        View Albums
+      </Button>
       <Button className={styles.visualButton} color="Info">
         View Songs
       </Button>
